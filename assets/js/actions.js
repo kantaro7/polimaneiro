a9460f483e1687181759b7f5d5ce5ab369dfb8d5
Gustavo Ordaz <ordazsgustavo@gmail.com>
2017-03-07 18:37:25 -0400

    Dropzone
    eliminado plugin

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -43,32 +43,4 @@ $(document).ready(function(){
         });
         return true;
     });
-
-    Dropzone.options.myAwesomeDropzone = {
-
-        autoProcessQueue: false,
-        uploadMultiple: true,
-        parallelUploads: 100,
-        maxFiles: 100,
-
-        // Dropzone settings
-        init: function() {
-            var myDropzone = this;
-
-            this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                myDropzone.processQueue();
-            });
-            this.on("sendingmultiple", function() {
-            });
-            this.on("successmultiple", function(files, response) {
-            });
-            this.on("errormultiple", function(files, response) {
-            });
-        }
-
-    }
-
-    $('#fotos').dropzone({ url: "/file/subir.php" });
 });
